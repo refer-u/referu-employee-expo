33070d770222e5296b5c285266d39014bf0136d3
nnaso0275q <nnaso0275@gmail.com>
2026-01-29 20:25:28 +0800

    backend

--- app/refer-history/_historyComponents/TabsPage.tsx
+++ app/refer-history/_historyComponents/TabsPage.tsx
@@ -1,45 +1,11 @@
+import { useAllReferrals } from "@/app/hook/use-all-referrals";
+import { formatDate } from "@/libs/utils/format-date";
 import * as NavigationBar from "expo-navigation-bar";
 import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Badge from "../_uiComponents/Badge";
 import CardItem from "../_uiComponents/CardItem";
 import TabButton from "../_uiComponents/TabButton";
-
-const historyData = {
-  sent: [
-    {
-      id: "1",
-      jobName: "Senior Software Engineer",
-      candidateName: "А. Дорж",
-      sentDate: "2024-01-15",
-    },
-    {
-      id: "2",
-      jobName: "Product Manager",
-      candidateName: "Б. Сарнай",
-      sentDate: "2024-01-12",
-    },
-  ],
-  approved: [
-    {
-      id: "3",
-      jobName: "UX Designer",
-      candidateName: "Ц. Болд",
-      sentDate: "2024-01-05",
-      approvedDate: "2024-01-10",
-      bonus: "₮500,000",
-    },
-  ],
-  rejected: [
-    {
-      id: "4",
-      jobName: "Data Analyst",
-      candidateName: "Д. Өсөх",
-      sentDate: "2023-12-20",
-      responseDate: "2023-12-28",
-    },
-  ],
-};
 
 type TabKey = "Илгээсэн" | "Зөвшөөрсөн" | "Цуцлагдсан";
 
@@ -49,6 +15,9 @@ export default function TabsPage() {
     NavigationBar.setBackgroundColorAsync("#5d3912");
     NavigationBar.setButtonStyleAsync("dark");
   }, []);
+
+  const { allReferrals } = useAllReferrals();
+  console.log("allReferrals", allReferrals);
   return (
     <View style={styles.container}>
       {/* Tabs */}
@@ -72,63 +41,82 @@ export default function TabsPage() {
       <ScrollView contentContainerStyle={styles.content}>
         {/* Илгээгдсэн */}
         {activeTab === "Илгээсэн" &&
-          historyData.sent.map((i) => (
-            <CardItem key={i.id} dotColor="#fb923c">
-              <Text style={styles.title}>{i.jobName}</Text>
-              <Text style={styles.muted}>
-                Санал болгосон: {i.candidateName}
-              </Text>
-              <View style={styles.badges}>
-                <Badge text={`Илгээсэн: ${i.sentDate}`} variant="Илгээсэн" />
-              </View>
-            </CardItem>
-          ))}
+          allReferrals
+            .filter((ref) => ref.referralStatus === "SUBMITTED")
+            .map((i) => (
+              <CardItem key={i._id} dotColor="#fb923c">
+                {/* <Text style={styles.title}>{i.jobName}</Text> */}
+                <Text style={styles.muted}>
+                  Санал болгосон: {i.candidateFirstName}
+                </Text>
+                <View style={styles.badges}>
+                  <Badge
+                    text={`Илгээсэн: ${formatDate(i.createdAt)}`}
+                    variant="Илгээсэн"
+                  />
+                </View>
+              </CardItem>
+            ))}
 
         {/* Зөвшөөрсөн */}
         {activeTab === "Зөвшөөрсөн" &&
-          historyData.approved.map((i) => (
-            <CardItem key={i.id} dotColor="#22c55e">
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text style={styles.title}>{i.jobName}</Text>
-                <Text style={styles.bonus}>{i.bonus}</Text>
-              </View>
-              <Text style={styles.muted}>
-                Санал болгосон: {i.candidateName}
-              </Text>
+          allReferrals
+            .filter(
+              (ref) =>
+                ref.referralStatus === "BONUS100" ||
+                ref.referralStatus === "BONUS200",
+            )
+            .map((i) => (
+              <CardItem key={i._id} dotColor="#22c55e">
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text style={styles.muted}>
+                    Санал болгосон: {i.candidateFirstName}
+                  </Text>
 
-              <View style={styles.badges}>
-                <Badge text={`Илгээсэн: ${i.sentDate}`} variant="Илгээсэн" />
+                  <Text style={styles.bonus}>{i.bonusAmount}₮</Text>
+                </View>
 
-                <Badge
-                  text={`Зөвшөөрсөн: ${i.approvedDate}`}
-                  variant="Зөвшөөрсөн"
-                />
-              </View>
-            </CardItem>
-          ))}
+                <View style={styles.badges}>
+                  <Badge
+                    text={`Илгээсэн: ${formatDate(i.createdAt)}`}
+                    variant="Илгээсэн"
+                  />
+
+                  <Badge
+                    text={`Зөвшөөрсөн: ${formatDate(i.updatedAt)}`}
+                    variant="Зөвшөөрсөн"
+                  />
+                </View>
+              </CardItem>
+            ))}
 
         {/* Цуцлагдсан */}
         {activeTab === "Цуцлагдсан" &&
-          historyData.rejected.map((i) => (
-            <CardItem key={i.id} dotColor="#ef4444">
-              <Text style={styles.title}>{i.jobName}</Text>
-              <Text style={styles.muted}>
-                Санал болгосон: {i.candidateName}
-              </Text>
-              <View style={styles.badges}>
-                <Badge text={`Илгээсэн: ${i.sentDate}`} variant="Илгээсэн" />
-                <Badge
-                  text={`Цуцлагдсан: ${i.responseDate}`}
-                  variant="Цуцлагдсан"
-                />
-              </View>
-            </CardItem>
-          ))}
+          allReferrals
+            .filter((ref) => ref.referralStatus === "REJECTED")
+            .map((i) => (
+              <CardItem key={i._id} dotColor="#ef4444">
+                {/* <Text style={styles.title}>{i.jobName}</Text> */}
+                <Text style={styles.muted}>
+                  Санал болгосон: {i.candidateFirstName}
+                </Text>
+                <View style={styles.badges}>
+                  <Badge
+                    text={`Илгээсэн: ${formatDate(i.createdAt)}`}
+                    variant="Илгээсэн"
+                  />
+                  <Badge
+                    text={`Цуцлагдсан: ${formatDate(i.updatedAt)}`}
+                    variant="Цуцлагдсан"
+                  />
+                </View>
+              </CardItem>
+            ))}
       </ScrollView>
     </View>
   );
